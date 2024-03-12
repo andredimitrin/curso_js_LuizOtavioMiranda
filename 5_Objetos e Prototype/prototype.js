1a1b76bea87942c51f3bca7754aa60f9a4efcc55
André Dimitrin <82331544+andredimitrin@users.noreply.github.com>
2024-03-11 22:31:20 -0300

    att

--- 5_Objetos e Prototype/prototype.js
+++ 5_Objetos e Prototype/prototype.js
@@ -2,13 +2,20 @@
 function Pessoa (nome, sobrenome){
     this.nome = nome;
     this.sobrenome = sobrenome;
-    this.nomeCompleto = () => this.nome + ' ' + this.sobrenome;
+    this.nomeCompleto = () => 'Original:' + this.nome + ' ' + this.sobrenome;
+}
+
+//prototype
+//Pessoa.prototype === pessoa1.__proto__
+
+Pessoa.prototype.nomeCompleto = function(){
+    return this.nome + ' ' + this.sobrenome
 }
 
 //instancia
 
-const pessoa1 = new Pessoa('Luiz', 'Ota');// <- Pessoa = função construtora
+const pessoa1 = new Pessoa('Luiz', 'Ota'); // <- Pessoa = função construtora
 const data = new Date();// <- Date = função construtora
 
-console.dir(pessoa1)
-console.dir(data)
+console.log(pessoa1)
+console.log(data)
